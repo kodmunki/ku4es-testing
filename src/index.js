4b81fc364d815d71ff30e3adea0650b23d38a4a7
kodmunki <kodmunki@gmail.com>
2018-03-06 08:03:45 -0600

    Expose

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import performance from './performance';
+import Performance from './performance';
 import { startServer, stopServer, sendResponse } from './network';
 import Response from './network/Response';
 
 export {
-  performance,
   startServer,
   stopServer,
   sendResponse,
+  Performance,
   Response
 };
---
+++ test/index.test.js
@@ -0,0 +1,15 @@
+import assert from 'assert';
+import { describe, it } from 'mocha';
+import * as index from '../src/index';
+
+describe('index Test', () => {
+
+  it('should expose', () => {
+    assert.ok(index.startServer);
+    assert.ok(index.stopServer);
+    assert.ok(index.sendResponse);
+    assert.ok(index.Performance);
+    assert.ok(index.Response);
+  });
+
+});
