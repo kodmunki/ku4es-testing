67066f0848d1d8f4b64ce0e94d67a88d2d309793
kodmunki <kodmunki@gmail.com>
2018-03-06 07:56:27 -0600

    DI Performance class

--- src/performance.js
+++ src/performance.js
@@ -1,9 +1,24 @@
-import { performance } from 'perf_hooks';
+export default class Performance {
 
-export default (func, iterations = 10000) => {
-  const p = performance;
-  const start = p.now();
-  while(iterations--) { func(); }
-  const end = p.now();
-  return end - start;
-};
+  /**
+   * @constructor Performance
+   * @param {Performance|DateConstructor} [nowable=DateConstructor] - Any object with method `now` returning a time of type number.
+   */
+  constructor(nowable = Date) {
+    this._nowable = nowable;
+  }
+
+  /**
+   * @constructor Performance
+   * @param func
+   * @param iterations
+   * @returns {number}
+   */
+  test(func, iterations = 10000) {
+    const start = this._nowable.now();
+    while(iterations--) { func(); }
+    const end = this._nowable.now();
+    return end - start;
+  }
+
+}
--- test/performance.test.js
+++ test/performance.test.js
@@ -1,17 +1,68 @@
 import assert from 'assert';
 import { describe, it } from 'mocha';
-import performance from '../src/performance';
+import Performance from '../src/performance';
 
-describe('performance Test', () => {
+describe('performance Tests with implicit Date', () => {
 
-  it('should pass', () => {
-    const test = () => 1 + 1;
-    assert.ok(performance(test) < 10);
+  const performance = new Performance();
+
+  it('should pass with Date', () => {
+    const sum = () => 1 + 1;
+    assert.ok(performance.test(sum) < 10);
   });
 
-  it('should fail', () => {
-    const test = () => { for(let i = 0; i < 100000; i++) { Math.pow(i, i); } };
-    assert.ok(!performance(test) < 400);
+  it('should fail with Date', () => {
+    const loop = () => { for(let i = 0; i < 100000; i++) { Math.pow(i, i); } };
+    assert.ok(!performance.test(loop) < 400);
   });
+
+});
+
+describe('performance Tests with explicit Date', () => {
+
+  const performance = new Performance(Date);
+
+  it('should pass with Date', () => {
+    const sum = () => 1 + 1;
+    assert.ok(performance.test(sum) < 10);
+  });
+
+  it('should fail with Date', () => {
+    const loop = () => { for(let i = 0; i < 100000; i++) { Math.pow(i, i); } };
+    assert.ok(!performance.test(loop) < 400);
+  });
+
+});
+
+describe('performance Tests with perf_hooks', () => {
+
+  try {
+
+    const {performance: nowable} = require('perf_hooks');
+    const performance = new Performance(nowable);
+
+    it('should pass with Date', () => {
+      const sum = () => 1 + 1;
+      assert.ok(performance.test(sum) < 10);
+    });
+
+    it('should fail with Date', () => {
+      const loop = () => {
+        for (let i = 0; i < 100000; i++) {
+          Math.pow(i, i);
+        }
+      };
+      assert.ok(!performance.test(loop) < 400);
+    });
+
+  }
+  catch (e) {
+
+    it('should notify no perf_hooks', () => {
+      console.info('    Module perf_hooks are unavailable in this environment.'); // eslint-disable-line no-console
+      assert.ok(true);
+    });
+
+  }
 
 });
