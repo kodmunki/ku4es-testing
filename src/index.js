f40ecf02a35cf2bdda07f34b092c356da096ec2b
kodmunki <kodmunki@gmail.com>
2018-03-05 21:05:46 -0600

    expose performance

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import performance from './performance';
 import { startServer, stopServer, sendResponse } from './network';
 import Response from './network/Response';
 
 export {
+  performance,
   startServer,
   stopServer,
   sendResponse,
