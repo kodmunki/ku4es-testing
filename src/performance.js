e5f6ef0a71ba80dd38a72d2777bab40173ba107a
kodmunki <kodmunki@gmail.com>
2018-03-06 07:25:19 -0600

    Add performance

--- src/performance.js
+++ src/performance.js
@@ -1,6 +1,9 @@
+import { performance } from 'perf_hooks';
+
 export default (func, iterations = 10000) => {
-  const start = Date.now();
+  const p = performance;
+  const start = p.now();
   while(iterations--) { func(); }
-  const end = Date.now();
+  const end = p.now();
   return end - start;
 };
